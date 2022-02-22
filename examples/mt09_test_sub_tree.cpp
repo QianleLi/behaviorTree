fb412d2d031221f461796900c09d07f4f1ce38d7
QianleLi <liqianle961116@163.com>
2022-02-22 18:17:27 +0800

    add test sub tree

---
+++ examples/mt09_test_sub_tree.cpp
@@ -0,0 +1,89 @@
+//
+// Created by Qianle Li on 2022/2/22.
+//
+
+#include "behaviortree_cpp_v3/loggers/bt_cout_logger.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "movebase_node.h"
+#include "dummy_nodes.h"
+
+//TODO 本测试证明XML中这样写接口是不行的，可以对照t06
+
+// clang-format off
+
+static const char* xml_text = R"(
+<root main_tree_to_execute = "MainTree">
+
+    <BehaviorTree ID="MainTree">
+
+        <Sequence name="main_sequence">
+            <SetBlackboard output_key="MoveGoal" value="1;2;3" />
+            <SubTree ID="MoveRobot" MoveGoal="MoveGoal" MoveResult="MoveResult" />
+            <SaySomething message="{MoveResult}"/>
+        </Sequence>
+
+    </BehaviorTree>
+
+    <BehaviorTree ID="MoveRobot">
+        <Fallback name="move_robot_main">
+            <SequenceStar>
+                <MoveBase       MoveGoal="MoveGoal"/>
+                <SetBlackboard output_key="MoveResult" value="mission accomplished" />
+            </SequenceStar>
+            <ForceFailure>
+                <SetBlackboard output_key="MoveResult" value="mission failed" />
+            </ForceFailure>
+        </Fallback>
+    </BehaviorTree>
+
+</root>
+ )";
+
+// clang-format on
+
+
+using namespace BT;
+using namespace DummyNodes;
+
+int main()
+{
+    BehaviorTreeFactory factory;
+
+    factory.registerNodeType<SaySomething>("SaySomething");
+    factory.registerNodeType<MoveBaseAction>("MoveBase");
+
+    auto tree = factory.createTreeFromText(xml_text);
+
+    NodeStatus status = NodeStatus::RUNNING;
+    // Keep on ticking until you get either a SUCCESS or FAILURE state
+    while( status == NodeStatus::RUNNING)
+    {
+        status = tree.tickRoot();
+        SleepMS(1);   // optional sleep to avoid "busy loops"
+    }
+
+    // let's visualize some information about the current state of the blackboards.
+    std::cout << "--------------" << std::endl;
+    tree.blackboard_stack[0]->debugMessage();
+    std::cout << "--------------" << std::endl;
+    tree.blackboard_stack[1]->debugMessage();
+    std::cout << "--------------" << std::endl;
+
+    return 0;
+}
+
+/* Expected output:
+
+    [ MoveBase: STARTED ]. goal: x=1 y=2.0 theta=3.00
+    [ MoveBase: FINISHED ]
+    Robot says: mission accomplished
+    --------------
+    move_result (std::string) -> full
+    move_goal (Pose2D) -> full
+    --------------
+    output (std::string) -> remapped to parent [move_result]
+    target (Pose2D) -> remapped to parent [move_goal]
+    --------------
+
+*/
